025854613445dab5fdd5e5668f8e3ae56b5fde03
eliasjcm <eliasc20022002@gmail.com>
2021-02-04 00:28:58 -0600

    Update main.c

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main() {
         printf("3. Listar jugadores por altura.\n");
         printf("4. Terminar programa.\n\n");
 
-        printf("\nIngrese la opción que desea: ");
+        printf("\nIngrese la opciÃ³n que desea: ");
         scanf("%d", &opcionUsuario);
         switch (opcionUsuario) {
             case 1:
@@ -80,51 +80,3 @@ int main() {
     } while (opcionUsuario != 4);
     return 0;
 }
-
-/*
-
-int main() {
-    float numerito;
-    scanf("%f", &numerito);
-    printf("%f", numerito);
-    return 0;
-}
-
-*/
-/*
-int main() {
-    struct jugador listaJugadores[2];
-    float numeros[5];
-    float tempValue = 0;
-    for (int i = 0; i < 2; i++) {
-        //scanf("%f", &tempValue);
-        //numeros[i] = tempValue;
-        scanf("%f", &listaJugadores[i].altura);
-    }
-
-    for (int i = 0; i < 2; i++) {
-        printf("%f\n", listaJugadores[i].altura);
-    }
-
-    return 0;
-}
-*/
-/*
-int main()
-{
-   float floatArray[10];
-   int i;
-   //input in array using for loop
-   for(i=0; i<10; i++)
-      {
-	   printf("Enter Element No:%d=",i+1);
-       scanf("%f", &floatArray[i]);
-      }
-   // show array elements using for loop
-    printf("The elements of array are:\n");
-    for(i=0;i<10;i++)
-      printf("%.2f\n", floatArray[i]);
-
-   return 0;
-}
-*/
